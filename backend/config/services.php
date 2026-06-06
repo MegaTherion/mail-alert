f48619ece1f00d98c4db6846615bc65d59cd1b3f
Christian <christian.gomez.4@gmail.com>
2026-06-05 21:48:46 -0400

    services agregado alert y fcm

--- backend/config/services.php
+++ backend/config/services.php
@@ -35,4 +35,13 @@ return [
         ],
     ],
 
+    'alert' => [
+        'secret' => env('ALERT_SECRET'),
+    ],
+
+    'fcm' => [
+        'project_id' => env('FCM_PROJECT_ID'),
+        'service_account_json' => env('FCM_SERVICE_ACCOUNT_JSON'),
+    ],
+
 ];
